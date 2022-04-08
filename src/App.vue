cc5bc61c70620960a48b8906e1db94d1d4900658
tons <294343714@qq.com>
2022-04-08 15:50:57 +0800

    4.8

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <hello msg="hello worlds" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Hello from './components/Hello'
+export default defineComponent({
+  name: 'App',
+  components:{
+    Hello
+  }
+})
+</script>
---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+interface HelloProps {
+  msg: string;
+}
+
+const Hello = (props: HelloProps)=>{
+    return(
+      <h1>{props.msg}</h1>
+    )
+}
+
+Hello.props = ['msg']
+
+export default Hello
